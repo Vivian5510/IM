a4ad6e6bc69ca4e027f134f108c09be4dcba2c32
Xiang Xin Kai <2156722358@qq.com>
2025-02-14 23:23:01 +0800

    4. 引入依赖，配置Nacos，注册服务

--- IM-Backend/im-api/im-auth-service/src/main/java/com/rosy/AuthApplication.java
+++ IM-Backend/im-api/im-auth-service/src/main/java/com/rosy/AuthApplication.java
@@ -3,8 +3,10 @@ package com.rosy;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @MapperScan("com.rosy.**.mapper")
 public class AuthApplication {
     public static void main(String[] args) {
--- IM-Backend/im-api/im-main-service/src/main/java/com/rosy/MainApplication.java
+++ IM-Backend/im-api/im-main-service/src/main/java/com/rosy/MainApplication.java
@@ -3,9 +3,11 @@ package com.rosy;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @MapperScan("com.rosy.**.mapper")
 public class MainApplication {
     public static void main(String[] args) {
