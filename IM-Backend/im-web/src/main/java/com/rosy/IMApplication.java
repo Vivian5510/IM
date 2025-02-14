3fa647fb09b503f01334ba98bf58dde485ef92b5
Xiang Xin Kai <2156722358@qq.com>
2025-02-14 20:10:59 +0800

    1. 初始化项目

---
+++ IM-Backend/im-web/src/main/java/com/rosy/IMApplication.java
@@ -0,0 +1,17 @@
+package com.rosy;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+@ConfigurationPropertiesScan("com.rosy.framework.config")
+@EnableAspectJAutoProxy
+@MapperScan("com.rosy.**.mapper")
+public class IMApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IMApplication.class, args);
+    }
+}
